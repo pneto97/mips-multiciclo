30db48ee8876d5e9278f30b33780b92bb9a734d7
pneto97 <paulo_neto@mail.com>
2018-12-10 22:30:08 -0200

    tratando signed no resize

--- resize32_16.vhd
+++ resize32_16.vhd
@@ -10,11 +10,13 @@ entity resize32_16 is
 	(
 
 		rs_in	   : in std_logic_vector(15 downto 0);
+	   sel			: in std_logic; -- 1 lh, 0 lhu
 		rs_out	: out std_logic_vector(WSIZE-1 downto 0)
 	);
 end entity;
 
 architecture rtl of resize32_16 is
 begin
-	rs_out <= std_logic_vector(resize(unsigned(rs_in), rs_out'length));
+	rs_out <= std_logic_vector(resize(unsigned(rs_in), rs_out'length)) when (sel = '1') else
+				std_logic_vector(resize(signed(rs_in), rs_out'length));
 end rtl;
